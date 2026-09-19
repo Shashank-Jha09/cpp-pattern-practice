8ddd1f76712765f2f55b8432fbb336d2896e0eb7
Shashank-Jha09 <shashank200928@gmail.com>
2026-09-19 23:15:17 +0530

    Add initial pattern programs

---
+++ square.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std; 
+void squr(int n){ 
+    for (int i=1; i<=n;i++ ){ 
+        for (int j=1; j<=n; j++){ 
+            cout<<'*'; 
+        }
+        cout<<endl; 
+    }
+}
+int main(){ 
+    int n; 
+    cin>>n; 
+    squr(n); 
+}
